81a05be6741ffc605c60c8c95e2199aab6e2b99d
Roman Minkin <minkinroman@gmail.com>
2014-03-31 22:01:14 -0400

    Explorations 2

--- example/server.js
+++ example/server.js
@@ -2,9 +2,9 @@
 
 var jade            = require('gulp-jade'),
     inject          = require('gulp-inject'),
-    cache           = require('gulp-cached'),
     path            = require('path'),
     util            = require('util'),
+    cache           = require('gulp-cache'),
     express         = require('express'),
     app             = express(),
     assetsFormatter = require('../lib/gulp-assets.js'),
@@ -16,23 +16,23 @@ function assetsTransformerJs(filepath, file, index, length) {
     return util.format( '<script src="%s"></script>', filepath.replace( '/client', '' ));
 }
 
-function assetsTransformerCSS(filepath, file, index, length) {
+function assetsTransformerCss(filepath, file, index, length) {
     return util.format( '<link rel="stylesheet" href="%s">', filepath.replace( '/client', '' ));
 }
 
 
 function getDesktopView(res) {
-    var js   = sapphire.asset.src('./client/js/**/*.js', {read: false}),
-        css  = sapphire.asset.src('./client/css/**/*.css', {base: '/client/', read: false});
+    var js     = sapphire.asset.src('./client/js/**/*.js', {read: false}),
+        css    = sapphire.asset.src('./client/css/**/*.css', {read: false});
 
         // prepearing the view
         sapphire.asset.src('./client/views/desktop.jade')
-            .pipe(cache('view'))
-            .pipe(jade({locals: {date: new Date()}}))
+            .pipe(cache(jade({locals: {date: new Date()}})))
             .pipe(inject(js,  {starttag: '<!-- inject:head:{{ext}} -->', transform: assetsTransformerJs }))
-            .pipe(inject(css, {starttag: '<!-- inject:head:{{ext}} -->', transform: assetsTransformerCSS }))
+            .pipe(inject(css, {starttag: '<!-- inject:head:{{ext}} -->', transform: assetsTransformerCss }))
             // sending compiled result to the client
             .on('data', function(data) {
+                // console.log('data', data);
                 data.pipe(res);
             });
 }
